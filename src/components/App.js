647acb0b38533ee14a91ec316dfbf0b0c180391e
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-01-05 03:16:54 +0900

    make a base about Route

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { authService } from "fbase";
+import AppRouter from "components/Router"
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? (<AppRouter isLoggedIn = {isLoggedIn}/>) : ("initializing...")}
+      <footer>&copy; {new Date().getFullYear()} Webtoon mangement</footer>
+    </>
+  );
+}
+
+export default App;
