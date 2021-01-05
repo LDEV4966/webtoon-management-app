4da73130e49e00e606d003e90d5d9e7b12c78833
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-01-06 01:12:06 +0900

    set the data to array

--- src/routes/Naver.js
+++ src/routes/Naver.js
@@ -3,28 +3,34 @@ const Naver = () => {
   const axios = require("axios");
   const cheerio = require("cheerio");
   let URL = `/webtoon/weekday.nhn`;
-  const [dataInit,setDataInit] = useState(true);
-  useEffect(()=>{
-      if(dataInit===true){
-        getHtml();
-        setDataInit(false);
-      }
-  })
+  const [dataInit, setDataInit] = useState(true);
+  const [webtoons, setWebtoons] = useState([]);
+  let toons = [];
+  useEffect(() => {
+    let all = [];
+    if (dataInit === true) {
+      getHtml();
+      setDataInit(false);
+    }
+  });
   const getHtml = async () => {
-    console.log("hi");
     const html = await axios.get(URL);
     const $ = await cheerio.load(html.data);
-    $("div.daily_all div.col div.col_inner ul li").each((i,element)=>{
-        const title = $(element).find("div.thumb a img").attr("title");
-        const img = $(element).find("div.thumb a img").attr("src");
-        let link = $(element).find("a.title").attr("href");
-        link = `https://comic.naver.com${link}`;
-        const day =  link.slice(-3);
-        console.log(title);
-        console.log(link);
-        console.log(day);
-        console.log(img);
-    })
+    $("div.daily_all div.col div.col_inner ul li").each((i, element) => {
+      const title = $(element).find("div.thumb a img").attr("title");
+      const img = $(element).find("div.thumb a img").attr("src");
+      let link = $(element).find("a.title").attr("href");
+      link = `https://comic.naver.com${link}`;
+      const day = link.slice(-3);
+      let webtoon = {
+        title: title,
+        img: img,
+        link: link,
+        day: day,
+      };
+      toons = [...toons, webtoon];
+    });
+    console.log(toons);
   };
   return <div>Naver</div>;
 };
