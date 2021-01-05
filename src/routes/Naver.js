de93af11c5e5c748c7042bf1604934a056bbcc83
leeseungmin4966 <40168455+leeseungmin4966@users.noreply.github.com>
2021-01-06 00:23:53 +0900

    scrapp the data from url

--- src/routes/Naver.js
+++ src/routes/Naver.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const Naver = () => {
-    return(
-        <div>
-            Naver
-        </div>
-    );
-}
+  const axios = require("axios");
+  const cheerio = require("cheerio");
+  let URL = `/webtoon/weekday.nhn`;
+  const [dataInit,setDataInit] = useState(true);
+  useEffect(()=>{
+      if(dataInit===true){
+        getHtml();
+        setDataInit(false);
+      }
+  })
+  const getHtml = async () => {
+    console.log("hi");
+    const html = await axios.get(URL);
+    const $ = await cheerio.load(html.data);
+    $("div.daily_all div.col div.col_inner ul li").each((i,element)=>{
+        const title = $(element).find("div.thumb a img").attr("title");
+        const img = $(element).find("div.thumb a img").attr("src");
+        let link = $(element).find("a.title").attr("href");
+        link = `https://comic.naver.com${link}`;
+        const day =  link.slice(-3);
+        console.log(title);
+        console.log(link);
+        console.log(day);
+        console.log(img);
+    })
+  };
+  return <div>Naver</div>;
+};
 export default Naver;
